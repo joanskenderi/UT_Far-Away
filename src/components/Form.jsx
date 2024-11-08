9e8a2fb35f0cf6fbb1b1a46c85be3e1fc8bd7dea
joanskenderi <jo.skenderi@gmail.com>
2024-11-08 21:00:33 +0100

    feat: add form logic

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,48 @@
+import { useState } from 'react';
+
 const Form = () => {
+  const [item, setItem] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!item) {
+      return;
+    }
+
+    const newItem = {
+      id: Date.now(),
+      item,
+      quantity,
+      packed: false,
+    };
+
+    setItem('');
+    setQuantity(1);
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, idx) => idx + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+      />
+      <button>Add Item</button>
+    </form>
   );
 };
 
